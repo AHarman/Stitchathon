219f6f7665f74e6542cc5a682c43227be1a10f02
Alexander Harman <alexanderjharman@gmail.com>
2018-01-10 17:45:48 +0000

    Can precompute 'distance' between colours to save on work

--- app/src/main/java/com/alexharman/stitchathon/ImageReader.kt
+++ app/src/main/java/com/alexharman/stitchathon/ImageReader.kt
@@ -17,13 +17,12 @@ class ImageReader {
 
     private fun quantizeColours(bitmap: Bitmap, numColours: Int): Bitmap {
         val colourCount = countColours(bitmap)
-        if (colourCount.keys.size <= numColours) {
-            return bitmap
-        }
+        if (colourCount.keys.size <= numColours) return bitmap
+        val distancetable = createDistanceTable(colourCount.keys.toTypedArray())
 
         // Do more processing here if needed
         Log.d("quantizing", "We have ${colourCount.keys.size} colours")
-        Log.d("quantizing", "We want to reduce that to $numColours")
+        Log.d("quantizing", "We want to reduce that to ${numColours}")
         return bitmap
     }
 
@@ -40,6 +39,24 @@ class ImageReader {
             }
         }
         return colourCount
+    }
+
+    private fun createDistanceTable(colours: Array<Colour>): HashMap<Colour, ArrayList<Colour>> {
+        val distanceTable = HashMap<Colour, ArrayList<Colour>>()
+        var row: ArrayList<Colour>
+        for (colour in colours) {
+            row = ArrayList()
+            row.addAll(colours.filter { it != colour }.sortedBy { colourDistance(it, colour) } )
+            distanceTable[colour] = row
+        }
+        return distanceTable
+    }
+
+    private fun colourDistance(c1: Colour, c2:Colour): Double {
+        val rDist = c1.r - c2.r
+        val gDist = c1.g - c2.g
+        val bDist = c1.b - c2.b
+        return sqrt((rDist*rDist + gDist*gDist + bDist*bDist).toDouble())
     }
 
     private data class Colour(val r: Int, val g: Int, val b: Int) {
