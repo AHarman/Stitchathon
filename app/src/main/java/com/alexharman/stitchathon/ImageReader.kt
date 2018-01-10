80ae11d99394ade81eb9cc3c3f10ac7e28ae6cab
Alexander Harman <alexanderjharman@gmail.com>
2018-01-10 17:30:07 +0000

    created colourCount structure

--- app/src/main/java/com/alexharman/stitchathon/ImageReader.kt
+++ app/src/main/java/com/alexharman/stitchathon/ImageReader.kt
@@ -1,9 +1,7 @@
 package com.alexharman.stitchathon
 
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.util.Log
-import kotlin.math.roundToInt
 import kotlin.math.sqrt
 
 /**
@@ -12,7 +10,42 @@ import kotlin.math.sqrt
 
 class ImageReader {
     fun readImage(bitmap: Bitmap, stitchesWide: Int, stitchesHigh: Int, numColours: Int): Bitmap {
-        val sampledBitmap = Bitmap.createScaledBitmap(bitmap, stitchesWide, stitchesHigh, true);
+        val sampledBitmap = Bitmap.createScaledBitmap(bitmap, stitchesWide, stitchesHigh, true)
+        quantizeColours(sampledBitmap, numColours)
         return sampledBitmap
+    }
+
+    private fun quantizeColours(bitmap: Bitmap, numColours: Int): Bitmap {
+        val colourCount = countColours(bitmap)
+        if (colourCount.keys.size <= numColours) {
+            return bitmap
+        }
+
+        // Do more processing here if needed
+        Log.d("quantizing", "We have ${colourCount.keys.size} colours")
+        Log.d("quantizing", "We want to reduce that to $numColours")
+        return bitmap
+    }
+
+    private fun countColours(bitmap: Bitmap): HashMap<Colour, Int> {
+        val colourCount: HashMap<Colour, Int> = HashMap()
+        var currentPixel: Colour
+        var currentPixelCount: Int
+
+        for (row in 0 until bitmap.height) {
+            for (col in 0 until bitmap.width) {
+                currentPixel = Colour(bitmap.getPixel(col, row))
+                currentPixelCount = colourCount[currentPixel] ?: 0
+                colourCount.put(currentPixel, currentPixelCount + 1)
+            }
+        }
+        return colourCount
+    }
+
+    private data class Colour(val r: Int, val g: Int, val b: Int) {
+        constructor(argb: Int) : this(
+                (argb and 0x00FF0000) shr 16,
+                (argb and 0x0000FF00) shr 8,
+                argb and 0x000000FF)
     }
 }
