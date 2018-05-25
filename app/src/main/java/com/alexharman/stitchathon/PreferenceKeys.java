cfbfb4e9bcc1ac484060f0ca05279e0bedae998e
Alexander Harman <alexanderjharman@gmail.com>
2018-05-25 02:39:59 +0100

    Created new file to put keys in

---
+++ app/src/main/java/com/alexharman/stitchathon/PreferenceKeys.java
@@ -0,0 +1,15 @@
+package com.alexharman.stitchathon;
+
+public final class PreferenceKeys {
+    public static final String STITCH_SIZE = "stitch_size";
+    public static final String STITCH_PAD = "stitch_padding";
+    public static final String BACKGROUND_COLOUR = "bg_colour";
+    public static final String STITCH_COLOUR_1 = "colour_1";
+    public static final String STITCH_COLOUR_2 = "colour_2";
+    public static final String STITCH_COLOUR_3 = "colour_3";
+    public static final String DELETE_ALL = "delete_all";
+    public static final String RESET_PREFS = "reset_preferences";
+    public static final String FIT_PATTERN_WIDTH = "fit_pattern_width";
+    public static final String LOCK_TO_SCREEN = "lock_to_current";
+    public static final String CURRENT_PATTERN_NAME = "pattern_name";
+}
